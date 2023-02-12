d3917ab4f955558ec93e1e74051b25ba747def67
TankVivek <vivektank136@gmail.com>
2023-02-12 03:33:12 -0500

    JavaScript

--- DSA/Contains_Duplicate.js
+++ DSA/Contains_Duplicate.js
@@ -21,20 +21,20 @@
 
 
 
-        let containsDuplicate = function(nums) {
-            if(nums.length<1){
-                return false;
-            }
-            for(let i=0;i<nums.length-1;i++){
-                let j=i+1;
-                while(j<nums.length){
-                    if(nums[i]===nums[j]){
-                        return true;
-                    }
-                    j++;
-                }
-            }
-            return false;
-        };
+        // let containsDuplicate = function(nums) {
+        //     if(nums.length<1){
+        //         return false;
+        //     }
+        //     for(let i=0;i<nums.length-1;i++){
+        //         let j=i+1;
+        //         while(j<nums.length){
+        //             if(nums[i]===nums[j]){
+        //                 return true;
+        //             }
+        //             j++;
+        //         }
+        //     }
+        //     return false;
+        // };
 
-        containsDuplicate( 1,2,3,4,4)
+        // containsDuplicate( 1,2,3,4,4)
