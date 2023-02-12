494d644eb606fdda714764f73b11d5491820b3bc
TankVivek <vivektank136@gmail.com>
2023-02-12 05:02:26 -0500

    JavaScript

--- DSA/Maximum_Subarray.js
+++ DSA/Maximum_Subarray.js
@@ -33,5 +33,17 @@
 
                //   Right Method
 
-
-               
+              
+               class Solution {
+                maxSubArray(nums) {
+                  let maxSum = Number.MIN_SAFE_INTEGER;
+                  let currSum = 0;
+                  
+                  for (let i = 0; i < nums.length; i++) {
+                    currSum = Math.max(currSum + nums[i], nums[i]);
+                    maxSum = Math.max(maxSum, currSum);
+                  }
+                  
+                  return maxSum;
+                }
+              }
