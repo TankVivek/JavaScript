f53cb376d48389fb063beb92c904d9501f5830dc
TankVivek <vivektank136@gmail.com>
2023-02-12 04:26:50 -0500

    JavaScript

--- DSA/Maximum_Subarray.js
+++ DSA/Maximum_Subarray.js
@@ -1,4 +1,37 @@
+            
+
+    // nums = [-2,1,-3,4,-1,2,1,-5,4];
+    // var b = 0;
+    // var c = 0;
+    // var d = 0;
+    // var e = 0;
+    // for( var i = 0 ; i < nums.length ; i++)
+    
+    // {
+     
+    //     if(true)
+    //     {
+    //           b = nums[i];
+    //           c = b + nums[i];
+
+    //           d = nums[i];
+    //           e = b + nums[i];
+
+              
+    //           if(c > e){
+                
+    //             console.log("is c");
+    //             console.log(c);
+    //           }
+    //           else
+        
+    //           {               
+    //                   console.log("print b");         
+    //                   console.log(b);
+    //       }}}
+   
+
+               //   Right Method
 
 
-    nums = [-2,1,-3,4,-1,2,1,-5,4]
-  
+               
