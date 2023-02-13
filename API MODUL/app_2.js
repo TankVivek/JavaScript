7c9f944e916333e607f762b513592223f5bf34ff
TankVivek <vivektank136@gmail.com>
2023-02-13 12:59:38 -0500

    JavaScript

---
+++ API MODUL/app_2.js
@@ -0,0 +1,14 @@
+const axios = require("axios")
+
+const url = "https://restcountries.com/v3.1/all"
+
+axios.get(url).then(result => {
+
+    for (var i = 0; i <= result.data.length; i++) {
+        const dt = result.data[i];
+        console.log(dt.name.common);
+    
+    }
+}).catch(err => {
+    console.log(err);
+})
