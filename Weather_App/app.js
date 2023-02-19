7ec7bad00cee92ac466e859d3898325ddcd5fd23
TankVivek <vivektank136@gmail.com>
2023-02-19 14:13:17 -0500

    javaScript

--- Weather_App/app.js
+++ Weather_App/app.js
@@ -3,8 +3,12 @@ const express = require ("express")
 const app = express();
 // const weather = require("./util/weather")
 // const geocode = require("./util/geocode")
-// const hbs = require ("hbs")
-// const path = require ("path")
+const hbs = require ("hbs")
+const path = require ("path")
 const port = 9005;
+
+app.get("/",(req,resp)=>{
+    resp.render("index",{user:"vivek"})
+})
 
 app.listen(port,()=>{ console.log("server is runnign" +port);})
