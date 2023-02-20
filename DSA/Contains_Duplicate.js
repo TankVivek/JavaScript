464e5dbd6f0ed0fa32c07bf86aad0ca4a43f0fda
TankVivek <vivektank136@gmail.com>
2023-02-20 13:03:38 -0500

    JavaScript

--- DSA/Contains_Duplicate.js
+++ DSA/Contains_Duplicate.js
@@ -2,17 +2,17 @@
 
      // first Method
 
-    // const a = [1,3,5,6] ;
-    // var b = 0;
+    const a = [1,3,5,6] ;
+    var b = 0;
 
-    // for( i = 0; i < a.length; i++ ) {
-    //     if(a[i] != b)
+    for( i = 0; i < a.length; i++ ) {
+        if(a[i] != b)
     
-    //     { b=a[i]  }
+        { b=a[i]  }
 
-    //      else
+         else
 
-    //      { console.log(true); }} 
+         { console.log(true); }} 
 
 
 
@@ -21,14 +21,14 @@
 
 
 
-        // let containsDuplicate = function(nums) {
-        //     if(nums.length<1){
+        // let containsDuplicate = function(a) {
+        //     if(a.length<1){
         //         return false;
         //     }
-        //     for(let i=0;i<nums.length-1;i++){
+        //     for(let i=0;i<a.length-1;i++){
         //         let j=i+1;
-        //         while(j<nums.length){
-        //             if(nums[i]===nums[j]){
+        //         while(j<a.length){
+        //             if(a[i]===a[j]){
         //                 return true;
         //             }
         //             j++;
