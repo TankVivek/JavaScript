632519c14e0faf5f4ff8a3a8f55b38e68ac7becd
TankVivek <vivektank136@gmail.com>
2023-02-27 14:00:54 -0500

    JavaScript

--- restApi/app.js
+++ restApi/app.js
@@ -1,12 +1,25 @@
 
-const express = require ("express")
-const app = express();
-const port = 9100;
-const mongoose = require("mongoose")
+   
+   const express = require ("express")
+    const app = express();
+    const port = 9100;
+    const mongoose = require("mongoose")
+    
+    
+    const durl = " mongodb://127.0.0.1:27017/student"
+    mongoose.connect(durl).then(()=>{
+        console.log("db connect");
+    }).catch(err => {
+        console.log(err);
+    })
 
+    const userSchema = new mongoose.Schema
+    
+    ({
+       
+    })
 
-
-
-app.listen(port, ()=> {
-    console.log("sever is runnig  :" +port);
-})
+    
+    
+    app.listen(port, ()=> {
+        console.log("sever is runnig  :" +port);  })
