f05b605354ee181d737b8b11d57377425313ba3d
TankVivek <vivektank136@gmail.com>
2023-03-02 04:12:22 -0500

    JavaScript

--- mongoDb/app.js
+++ mongoDb/app.js
@@ -9,33 +9,19 @@ MongoClient.connect(DBurl).then(result=>
     console.log("db connect");
 
     const myDB = result.db(databse)
-    myDB.createCollection("new account").then(data => {
-
-        console.log("data Created");
-    })
-
-}).catch(err => {console.log(err);})
 
 
-// const mongodb = require("mongodb")
-// const MongoClient = mongodb.MongoClient
+    // myDB.createCollection("new account").then(data => {
 
-// const dburl = "mongodb://127.0.0.1:27017/";
-// const database = "bank"
+    //     console.log("data Created");
+    // })
+    
+const em1 = { name : "vivek" , email : "vivektank@gmail.com" }
+const em2 = { name : "rutvik" , email : "rutvik@gmail.com" }
+const em3 = { name : "jay" , email : "jay@gmail.com" }
 
-// MongoClient.connect(dburl).then(result => { console.log("db connect");
+myDB.collection("emp").insertMany([em1,em2,em3]).then(result => {
+    console.log(result);}).catch(err => {
+    console.log(err);    
 
-
-// const mydb = result.db(database)
-
-
-// mydb.createCollection("Account").then(data => { console.log("collections crated");})}).catch(err => {console.log(err);})
-
-
-// const em1 = { name : "vivek" , email : "vivektank@gmail.com" }
-// const em2 = { name : "rutvik" , email : "rutvik@gmail.com" }
-// const em3 = { name : "jay" , email : "jay@gmail.com" }
-
-// mydb.collection("emp").insertMany([em1,em2,em3]).then(result => { 
-//     console.log(result);}).catch(err => {console.log(err);})
-
+})}).catch(err => {console.log(err);})
