dac7ad7511d3d64cb1c98356590b989cd8489201
TankVivek <vivektank136@gmail.com>
2023-03-02 03:54:16 -0500

    JavaScript

--- mongoDb/app.js
+++ mongoDb/app.js
@@ -7,18 +7,19 @@ const MongoClient = mongodb.MongoClient
 const dburl = "mongodb://127.0.0.1:27017/";
 const database = "bank"
 
-MongoClient.connect(dburl).then(result => { console.log("db connect");})
+MongoClient.connect(dburl).then(result => { console.log("db connect");
+
 
 const mydb = result.db(database)
 
 
-// mydb.createCollection("Account").then(result => { console.log(result);})
+mydb.createCollection("Account").then(data => { console.log("collections crated");})}).catch(err => {console.log(err);})
 
 
-const em1 = { name : "vivek" , email : "vivektank@gmail.com" }
-const em2 = { name : "rutvik" , email : "rutvik@gmail.com" }
-const em3 = { name : "jay" , email : "jay@gmail.com" }
+// const em1 = { name : "vivek" , email : "vivektank@gmail.com" }
+// const em2 = { name : "rutvik" , email : "rutvik@gmail.com" }
+// const em3 = { name : "jay" , email : "jay@gmail.com" }
 
-mydb.collection("emp").insertMany([em1,em2,em3]).then(result => { 
-    console.log(result);}).catch(err => {console.log(err);})
+// mydb.collection("emp").insertMany([em1,em2,em3]).then(result => { 
+//     console.log(result);}).catch(err => {console.log(err);})
 
