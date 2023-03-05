9723a19b0b12eeb9b0acc939a373712206a43ea1
TankVivek <vivektank136@gmail.com>
2023-03-05 11:52:58 -0500

    NodeJs

--- new add/app.js
+++ new add/app.js
@@ -1,9 +1,33 @@
 const express = require("express");
 const app = express();
-const port = 9005;
+const port = 9006;
+const mongoose = require("mongoose");
+const user = require("../Reast Api Postman/model/user");
+mongoose.set("strictQuery", true);
 
-app.listen(
-  (port = () => {
-    console.log("port running " + port);
+const Aurl =
+  "mongodb+srv://vivektank:vivektank@cluster0.pqvunzi.mongodb.net/new_File?retryWrites=true&w=majority";
+
+mongoose
+  .connect(Aurl)
+  .then((data) => {
+    console.log("db connect");
   })
-);
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/users", (req, resp) => {
+  user
+    .save(req.body)
+    .then((data) => {
+      resp.send(data);
+    })
+    .catch((err) => {
+      resp.send(err);
+    });
+});
+
+app.listen(port, () => {
+  console.log("port running " + port);
+});
