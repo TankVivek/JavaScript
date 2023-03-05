4cc9f63d2dc943cc19672236bbeea73ae78129ed
TankVivek <vivektank136@gmail.com>
2023-03-05 09:24:26 -0500

    NodeJs

--- Reast Api Postman/app.js
+++ Reast Api Postman/app.js
@@ -1,42 +1,49 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 const port = 9000;
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
+app.use(express.json());
+const url =
+  "mongodb+srv://vivektank:vivektank@cluster0.pqvunzi.mongodb.net/?retryWrites=true&w=majority";
 
-app.use(express.json())
-const url = "mongodb+srv://vivektank:vivektank@cluster0.pqvunzi.mongodb.net/?retryWrites=true&w=majority"
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("db connect");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-mongoose.connect(url).then(result => { console.log("db connect");}).catch(err => {console.log(err);})
+// const newSchema = new mongoose.Schema( {
 
+// name:{
+//     type:String
+// },
+// email:{
+//     type:String
+// },
+// _id:Number
 
-    // const newSchema = new mongoose.Schema( {
+// })
 
-    // name:{       
-    //     type:String
-    // },
-    // email:{
-    //     type:String
-    // },
-    // _id:Number
-
-    // })
-
-    // const User = new mongoose.model("data",newSchema)
+// const User = new mongoose.model("data",newSchema)
 
 // app.get("/users",(req,resp) => {
 //     User.find().then(data => {
 //          resp.send(data)
-//         }).catch(err => { 
+//         }).catch(err => {
 //             resp.send(err)
 //         })
 // })
 
 // app.post("/users",(req,resp) => {
 //     const us = new  User(req.body)
+
 //          us.save().then(data => {
 //      resp.send(data)
-//         }).catch(err => { 
+//         }).catch(err => {
 //             resp.send(err)
 //         })
 // })
@@ -45,7 +52,7 @@ mongoose.connect(url).then(result => { console.log("db connect");}).catch(err =>
 //     const _id = req.params.id
 //     User.findByIdAndUpdate(_id,req.body).then(data => {
 //         resp.send(data)
-//     }).catch(err => { 
+//     }).catch(err => {
 //         resp.send(err)
 //     })
 // })
@@ -54,7 +61,7 @@ mongoose.connect(url).then(result => { console.log("db connect");}).catch(err =>
 //     const _id = req.params.id
 //     User.findByIdAndUpdate(_id, req.body).then(data => {
 //         resp.send(data)
-//     }).catch(err => { 
+//     }).catch(err => {
 //         resp.send(err)
 //     })
 // })
@@ -65,10 +72,11 @@ mongoose.connect(url).then(result => { console.log("db connect");}).catch(err =>
 //     }).catch(err => { resp.send(err)})
 // })
 
-const userrouter = require("./router/userrouter")
-const productroutr = require("./router/productrouter")
-app.use("/", userrouter)
-app.use("/", productroutr)
+const userrouter = require("./router/userrouter");
+const productroutr = require("./router/productrouter");
+app.use("/", userrouter);
+app.use("/", productroutr);
 
-
-app.listen(port, () => { console.log("sever is runng");})
+app.listen(port, () => {
+  console.log("sever is runng");
+});
--- restApi/app.js
+++ restApi/app.js
@@ -1,79 +1,89 @@
-
-
-const express = require("express")
+const express = require("express");
 const app = express();
 const port = 9123;
-const mongoose = require("mongoose")
-const dburl = "mongodb://127.0.0.1:27017/Company"
+const mongoose = require("mongoose");
+const dburl = "mongodb://127.0.0.1:27017/Company";
 
-mongoose.set('strictQuery', true)
-app.use(express.json())
-mongoose.connect(dburl).then(result => {
+mongoose.set("strictQuery", true);
+app.use(express.json());
+mongoose
+  .connect(dburl)
+  .then((result) => {
     console.log("db connected");
-}).catch(err => {console.log(err);})
-
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-       
-    },
-    email: {
-        type: String,
-        
-    },
-    pass: {
-        type: String
-    },
-    
-})
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  pass: {
+    type: String,
+  },
+});
 
-const User = new mongoose.model("User", userSchema)
-
+const User = new mongoose.model("User", userSchema);
 
 app.get("/users", (req, resp) => {
-
-    User.find().then(data => {
-        resp.send(data)
-    }).catch(err => {
-        console.log(err);
-        resp.send(err)
+  User.find()
+    .then((data) => {
+      resp.send(data);
     })
+    .catch((err) => {
+      console.log(err);
+      resp.send(err);
+    });
+});
 
-})
-
-app.post("/users", (req,resp) => {
-
-    const user = new User(req.body)
-    user.save().then(data =>{
-         resp.send(data)
-    }).catch(err => {
-        resp.send(err)
+app.post("/users", (req, resp) => {
+  const user = new User(req.body);
+  user
+    .save()
+    .then((data) => {
+      resp.send(data);
     })
-})
-app.put("/users/:id",(req,resp) => {
-    const _id = req.params.id
-    User.findByIdAndUpdate(_id,req.body).then(data => {
-        resp.send(data)
-    }).catch(err => { resp.send(err)})
-})
-
-app.patch("/users/:id", (req,resp) => {
-    const _id = req.params.id
-    User.findByIdAndUpdate(_id, req.body).then(data => {
-        resp.send(data)
-    }).catch(err => { 
-        resp.send(err)
+    .catch((err) => {
+      resp.send(err);
+    });
+});
+app.put("/users/:id", (req, resp) => {
+  const _id = req.params.id;
+  User.findByIdAndUpdate(_id, req.body)
+    .then((data) => {
+      resp.send(data);
     })
-})
+    .catch((err) => {
+      resp.send(err);
+    });
+});
 
-app.delete("/users/:id", (req,resp) => {
-    const _id = req.params.id
-    User.findByIdAndDelete(_id, req.body).then(data => {
-        resp.send(data)
-    }).catch(err => { resp.send(err)})
-})
+app.patch("/users/:id", (req, resp) => {
+  const _id = req.params.id;
+  User.findByIdAndUpdate(_id, req.body)
+    .then((data) => {
+      resp.send(data);
+    })
+    .catch((err) => {
+      resp.send(err);
+    });
+});
+
+app.delete("/users/:id", (req, resp) => {
+  const _id = req.params.id;
+  User.findByIdAndDelete(_id, req.body)
+    .then((data) => {
+      resp.send(data);
+    })
+    .catch((err) => {
+      resp.send(err);
+    });
+});
 
 app.listen(port, () => {
-    console.log("sevser running" + port);
-})
+  console.log("sevser running" + port);
+});
