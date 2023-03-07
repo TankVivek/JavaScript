ed7da224e626d2798b18b333bdd3eda3d0a1b51a
TankVivek <vivektank136@gmail.com>
2023-03-07 12:45:38 -0500

    NodeJs

--- Reast Api Postman/router/productrouter.js
+++ Reast Api Postman/router/productrouter.js
@@ -1,48 +1,58 @@
+const express = require("express");
+const router = express.Router();
+const Product = require("../model/product");
 
-const express = require("express")
-const router = express.Router()
-const Product = require("../model/product")
-
-
-router.get("/product",(req,resp) => {
-    Product.find().then(data => {
-         resp.send(data)
-        }).catch(err => { 
-            resp.send(err)
-        })
-})
-
-router.post("/product",(req,resp) => {
-    const us = new  Product(req.body)
-         us.save().then(data => {
-     resp.send(data)
-        }).catch(err => { 
-            resp.send(err)
-        })
-})
-
-router.put("/product/:id",(req,resp) => {
-    const _id = req.params.id
-    Product.findByIdAndUpdate(_id,req.body).then(data => {
-        resp.send(data)
-    }).catch(err => { 
-        resp.send(err)
+router.get("/view", (req, resp) => {
+  Product.find()
+    .then((data) => {
+      resp.send(data);
     })
-})
+    .catch((err) => {
+      resp.send(err);
+    });
+});
 
-router.patch("/product/:id", (req,resp) => {
-    const _id = req.params.id
-    Product.findByIdAndUpdate(_id, req.body).then(data => {
-        resp.send(data)
-    }).catch(err => { 
-        resp.send(err)
+router.post("/add", (req, resp) => {
+  const us = new Product(req.body);
+  us.save()
+    .then((data) => {
+      resp.send(data);
     })
-})
-router.delete("/product/:id", (req,resp) => {
-    const _id = req.params.id
-    Product.findByIdAndDelete(_id, req.body).then(data => {
-        resp.send(data)
-    }).catch(err => { resp.send(err)})
-})
+    .catch((err) => {
+      resp.send(err);
+    });
+});
 
-module.exports = router
+router.put("/update/:id", (req, resp) => {
+  const _id = req.params.id;
+  Product.findByIdAndUpdate(_id, req.body)
+    .then((data) => {
+      resp.send(data);
+    })
+    .catch((err) => {
+      resp.send(err);
+    });
+});
+
+router.patch("/update/:id", (req, resp) => {
+  const _id = req.params.id;
+  Product.findByIdAndUpdate(_id, req.body)
+    .then((data) => {
+      resp.send(data);
+    })
+    .catch((err) => {
+      resp.send(err);
+    });
+});
+router.delete("/delete/:id", (req, resp) => {
+  const _id = req.params.id;
+  Product.findByIdAndDelete(_id, req.body)
+    .then((data) => {
+      resp.send(data);
+    })
+    .catch((err) => {
+      resp.send(err);
+    });
+});
+
+module.exports = router;
