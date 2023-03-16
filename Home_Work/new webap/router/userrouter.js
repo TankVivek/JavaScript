0d1c98a0e731c997b96828c5562877c0eab85301
TankVivek <vivektank136@gmail.com>
2023-03-16 15:14:42 -0400

    Node Js

---
+++ Home_Work/new webap/router/userrouter.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const router = express.router();
+const User = require("../model/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const auth = require("../middleware/auth");
+const multer = require("multer");
---
+++ Home_Work/new webap/src/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+mongoose = require("mongoose");
+const path = require("path");
+const hbs = require("hbs");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+
+const dbural =
+  "mongodb+srv://vivektank:vivektank@cluster0.pqvunzi.mongodb.net/new_database?retryWrites=true&w=majority";
+
+mongoose.connect(dbural).then((result) => {
+  console.log("db connected");
+});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+const publicpath = path.join(__dirname, "../public");
+const viewpath = path.join(__dirname, "../templetes/views");
+const partialpath = path.join(__dirname, "../templetes/partials");
+
+app.set("view engine", "hbs");
+app.set("views", viewpath);
+hbs.registerPartials(partialpath);
+app.use(express.static(publicpath));
+
+app.use("/", require("../router/userrouter"));
+
+app.listen(port, (req, resp) => {
+  console.log("port is running :" + port);
+});
