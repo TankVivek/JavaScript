accba599afed823a90b305f36f6bdd1214794379
TankVivek <vivektank136@gmail.com>
2023-03-17 14:00:01 -0400

    Node JS

--- Home_Work/new webap/router/userrouter.js
+++ Home_Work/new webap/router/userrouter.js
@@ -17,4 +17,47 @@ router.get("/login", (req, resp) => {
   resp.render("login");
 });
 
+router.post("/addUser", async (req, resp) => {
+  try {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      pass: req.body.pass,
+    });
+    const data = await user.save();
+    resp.render("registraion", { msg: "registration successfully" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/userLogin", async (req, resp) => {
+  try {
+    const useremail = req.body.eamil;
+    const userpass = req.body.pass;
+
+    const userdata = await User.findOne({ email: useremail });
+    const isValid = await bcrypt.compare(userpass, userdata.pass);
+    if (isValid) {
+      const userdata1 = await User.find();
+
+      const token = await userdata.generateToken();
+
+      resp.cookie("jwt", token);
+      resp.render("home", { user: userdata.uname, udata: userdata1 });
+    } else {
+      resp.render("login", { err: "Invalid credentials !!!" });
+    }
+  } catch (error) {
+    resp.render("login", { err: "Invalid credentials !!!" });
+  }
+});
+
+router.get("/home", async (req, resp) => {
+  try {
+    const userdata = await User.find();
+    resp.render("home", { udata: userdata });
+  } catch (error) {}
+});
+
 module.exports = router;
